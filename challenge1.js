38bd171daf4156971b77dbef15aa75f2308461a8
Furbixtubs <furbixdev@gmail.com>
2025-12-16 13:34:29 +0100

    Initial commit

---
+++ challenge1.js
@@ -0,0 +1,8 @@
+const fullName = "Tochukwu Samuel";
+const age = 23;
+const programmingLanguage = "JavaScript";
+const isStudent = true;
+const currentYear = 2025;
+const intro = `Hello, my name is ${fullName}. I am ${age} years old and I love programming in ${programmingLanguage}. It is ${isStudent} that I am a student. The current year is ${currentYear}.`;
+
+console.log(intro);
---
+++ challenge2.js
@@ -0,0 +1,15 @@
+let num1 = 20;
+let num2 = 30;
+let sum = num1 + num2;
+let subtraction = num2 - num1;
+let multiplication = num1 * num2;
+let division = num2 / num1;
+let modulus = num2 % num1;
+let exponentiation = num1 ** 2;
+
+console.log(sum);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+console.log(modulus);
+console.log(exponentiation);
